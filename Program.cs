a82b6dd3e9c6ff811baae8e661b8178e70afb9f6
vclong2003 <vclong2003@gmail.com>
2023-05-17 06:03:19 +0700

    clean program.cs

--- Program.cs
+++ Program.cs
@@ -5,12 +5,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<DataContext>(opt => opt.UseSqlServer("Server=tcp:1670.database.windows.net,1433;Initial Catalog=1670_db;Persist Security Info=False;User ID=admin123;Password=Admin1670;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+
 var app = builder.Build();
+
 app.UseCors(builder => builder
 .AllowAnyHeader()
 .AllowAnyMethod()
