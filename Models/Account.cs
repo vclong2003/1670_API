c2455ec3fc32040e0588cee6f4cd1718315e5bf9
vclong2003 <vclong2003@gmail.com>
2023-05-25 16:47:35 +0700

    Add  role validation

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,10 +1,14 @@
-﻿namespace _1670_API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _1670_API.Models
 {
     public class Account
     {
         public int Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
+
+        [RegularExpression("^(CUSTOMER|STAFF|MANAGER)$", ErrorMessage = "Invalid role")]
         public string Role { get; set; }
 
         public ICollection<Order> Orders { get; } = new List<Order>();
