dbc16afffd82113c8429f50d38036341b3b9d812
vclong2003 <vclong2003@gmail.com>
2023-06-01 03:34:45 +0700

    fix cart (tested)

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -15,127 +15,84 @@ namespace _1670_API.Controllers
         {
             _dataContext = dataContext;
         }
+
         [HttpGet]
-        public async Task<ActionResult> Get()
+        public async Task<ActionResult> GetAllCartItems()
         {
-
             AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
-            if (accountDTO == null)
-            {
-                return StatusCode(401, "Unauthorized");
-            }
-            else
-            {
-                var carts = await _dataContext.CartItems.Where(c => c.CustomerId == accountDTO.Id).
-                    Include(c => c.Product)
-                    .Select(c => new
-                    {
-                        name = c.Product.Name,
-                        quantity = c.Quantity,
-                        price = c.Product.Price
-                    })
-                    .ToListAsync();
-                return StatusCode(200, carts);
-            }
+            if (accountDTO == null) { return StatusCode(401, "Unauthorized"); }
+
+            var items = await _dataContext.CartItems.Where(c => c.CustomerId == accountDTO.Id).
+                Include(c => c.Product)
+                .Select(c => new
+                {
+                    id = c.Product.Id,
+                    name = c.Product.Name,
+                    price = c.Product.Price,
+                    quantity = c.Quantity,
+                })
+                .ToListAsync();
+
+            return StatusCode(200, items);
+
         }
+
         [HttpPost]
-        public async Task<ActionResult> Add(CartItemDTO cartItem)
+        public async Task<ActionResult> AddItemToCart(CartItemDTO cartItemDTO)
         {
             AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
-            if (accountDTO == null)
-            {
-                return StatusCode(401, "Unauthorized");
-            }
-            else
-            {
-                var current = await _dataContext.CartItems.FindAsync(accountDTO.Id, cartItem.ProductId);
-                if (current is null)
-                {
-                    int quantity = _dataContext.Products
-                        .Where(p => p.Id == cartItem.ProductId)
-                        .Select(p => p.Quantity)
-                        .SingleOrDefault();
-                    if (quantity < cartItem.Quantity)
-                    {
-                        return StatusCode(401, "Product is exceed to stock");
-                    }
-                    else
-                    {
-                        CartItem cart = new()
-                        {
-                            CustomerId = (int)accountDTO.Id,
-                            ProductId = (int)cartItem.ProductId,
-                            Quantity = (int)cartItem.Quantity
-                        };
+            if (accountDTO == null) { return StatusCode(401, "unauthorized"); }
 
-                        _dataContext.CartItems.Add(cart);
-                        await _dataContext.SaveChangesAsync();
-                        return StatusCode(200, "Add To Cart Successfully");
-                    }
-                }
-                else
-                {
-                    return StatusCode(401, "This product has existed in your cart");
-                }
-            }
+            var existedItem = await _dataContext.CartItems.FindAsync(accountDTO.Id, cartItemDTO.ProductId);
+            if (existedItem != null) { return StatusCode(400, "Product already existed in cart!"); }
+
+            var product = await _dataContext.Products.FindAsync(cartItemDTO.ProductId);
+            if (product.Quantity < cartItemDTO.Quantity) { return StatusCode(400, "Exceed products available!"); }
+
+            CartItem newItem = new()
+            {
+                CustomerId = (int)accountDTO.Id,
+                ProductId = (int)cartItemDTO.ProductId,
+                Quantity = (int)cartItemDTO.Quantity
+            };
+
+            _dataContext.CartItems.Add(newItem);
+            await _dataContext.SaveChangesAsync();
+
+            return StatusCode(200);
         }
+
         [HttpPut("{id}")]
-        public async Task<ActionResult> Update(int quantityAdded, int id)
+        public async Task<ActionResult> UpdateItemQuantity(int id, CartItemDTO cartItemDTO)
         {
             AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
-            if (accountDTO == null)
-            {
-                return StatusCode(401, "Unauthorized");
-            }
-            else
-            {
-                var current = await _dataContext.CartItems.FindAsync(accountDTO.Id, id);
-                if (current is null)
-                {
-                    return StatusCode(401, "This item is not exist");
-                }
-                else
-                {
-                    int quantity = _dataContext.Products
-                        .Where(p => p.Id == id)
-                        .Select(p => p.Quantity)
-                        .SingleOrDefault();
-                    if (quantity < quantityAdded)
-                    {
-                        return StatusCode(401, "Product is exceed to stock");
-                    }
-                    else
-                    {
-                        current.Quantity = quantityAdded;
-                        await _dataContext.SaveChangesAsync();
-                        return StatusCode(200, "Update Cart Item Successfully");
-                    }
-                }
-            }
+            if (accountDTO == null) { return StatusCode(401, "unauthorized"); }
+
+            var item = await _dataContext.CartItems.FindAsync(accountDTO.Id, id);
+            if (item == null) { return StatusCode(404, "Item not existed!"); }
+
+            var product = await _dataContext.Products.FindAsync(id);
+            if (product.Quantity < cartItemDTO.Quantity) { return StatusCode(400, "Exceed products available!"); }
+
+            item.Quantity = (int)cartItemDTO.Quantity;
+            await _dataContext.SaveChangesAsync();
+
+            return StatusCode(200);
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> Delete(int id)
         {
             AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
-            if (accountDTO == null)
-            {
-                return StatusCode(401, "Unauthorized");
-            }
-            else
-            {
-                CartItem current = await _dataContext.CartItems.FindAsync(accountDTO.Id, id);
-                if (current is null)
-                {
-                    return StatusCode(401, "This item is not exist");
-                }
-                else
-                {
-                    _dataContext.CartItems.Remove(current);
-                    await _dataContext.SaveChangesAsync();
-                    return StatusCode(200, "Delete item successfully");
-                }
-            }
+            if (accountDTO == null) { return StatusCode(401, "unauthorized"); }
+
+            CartItem item = await _dataContext.CartItems.FindAsync(accountDTO.Id, id);
+            if (item == null) { return StatusCode(404, "item-not-exist"); }
+
+            _dataContext.CartItems.Remove(item);
+            await _dataContext.SaveChangesAsync();
+
+            return StatusCode(200);
         }
     }
 }
