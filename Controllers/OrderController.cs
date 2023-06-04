8c36ea7a199e823361994f89563c1a2629d7f3bb
vclong2003 <vclong2003@gmail.com>
2023-06-04 16:57:35 +0700

    update database

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -2,7 +2,6 @@
 using _1670_API.Helpers;
 using _1670_API.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace _1670_API.Controllers
 {
@@ -17,56 +16,56 @@ namespace _1670_API.Controllers
             _dataContext = context;
         }
 
-        [HttpGet]
-        public async Task<ActionResult> GetOrders()
-        {
-            AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
-            if (accountDTO == null)
-            {
-                return StatusCode(401, "Unauthorized");
-            }
+        //[HttpGet]
+        //public async Task<ActionResult> GetOrders()
+        //{
+        //    AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
+        //    if (accountDTO == null)
+        //    {
+        //        return StatusCode(401, "Unauthorized");
+        //    }
 
-            var items = await _dataContext.Orders.Where(o => o.CustomerId == accountDTO.Id).
-                Include(o => o.ShippingAddress)
-                .Select(o => new
-                {
-                    id = o.Id,
-                    name = o.ShippingAddress.Name,
-                    address = o.ShippingAddress.Address,
-                    phone = o.ShippingAddress.Phone,
-                    city = o.ShippingAddress.City,
-                    country = o.ShippingAddress.Country,
-                })
-                .ToListAsync();
+        //    var items = await _dataContext.Orders.Where(o => o.CustomerId == accountDTO.Id).
+        //        Include(o => o.ShippingAddress)
+        //        .Select(o => new
+        //        {
+        //            id = o.Id,
+        //            name = o.ShippingAddress.Name,
+        //            address = o.ShippingAddress.Address,
+        //            phone = o.ShippingAddress.Phone,
+        //            city = o.ShippingAddress.City,
+        //            country = o.ShippingAddress.Country,
+        //        })
+        //        .ToListAsync();
 
-            return StatusCode(200, items);
-        }
+        //    return StatusCode(200, items);
+        //}
 
-        [HttpGet("{id}")]
-        public async Task<ActionResult> GetOrderItems(string id)
-        {
-            AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
-            if (accountDTO == null)
-            {
-                return StatusCode(401, "Unauthorized");
-            }
+        //[HttpGet("{id}")]
+        //public async Task<ActionResult> GetOrderItems(string id)
+        //{
+        //    AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
+        //    if (accountDTO == null)
+        //    {
+        //        return StatusCode(401, "Unauthorized");
+        //    }
 
-            var items = await _dataContext.OrderItems.Where(o => o.OrderId == id).
-                Include(o => o.Product)
-                .Select(o => new
-                {
-                    productName = o.Product.Name,
-                    price = o.Product.Price,
-                    quantity = o.Quantity,
-                    total = o.Product.Price * o.Quantity
-                })
-                .ToListAsync();
+        //    var items = await _dataContext.OrderItems.Where(o => o.OrderId == id).
+        //        Include(o => o.Product)
+        //        .Select(o => new
+        //        {
+        //            productName = o.Product.Name,
+        //            price = o.Product.Price,
+        //            quantity = o.Quantity,
+        //            total = o.Product.Price * o.Quantity
+        //        })
+        //        .ToListAsync();
 
-            return StatusCode(200, items);
-        }
+        //    return StatusCode(200, items);
+        //}
 
         // POST: api/order
-        // body params: shippingAddressId, shippingMethod
+        // body params: shippingAddressId, paymentMethod
         // return id of the new order
         [HttpPost]
         public async Task<ActionResult> CreateOrder(OrderDTO orderDTO)
@@ -86,7 +85,7 @@ namespace _1670_API.Controllers
                 ShippingAddressId = orderDTO.ShippingAddressId,
                 StaffId = null,
                 Date = DateTime.Now,
-                ShippingMethod = orderDTO.ShippingMethod,
+                PaymentMethod = orderDTO.PaymentMethod,
                 Status = "Pending"
             };
             _dataContext.Orders.Add(newOrder);
@@ -108,57 +107,56 @@ namespace _1670_API.Controllers
             return StatusCode(200, newOrder.Id);
         }
 
-        [HttpPut("{id}")]
-        public async Task<ActionResult> UpdateOrderStatus(string id, OrderStatusDTO orderStatus)
-        {
-            AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
-            if (accountDTO == null)
-            {
-                return StatusCode(401, "Unauthorized");
-            }
+        //[HttpPut("{id}")]
+        //public async Task<ActionResult> UpdateOrderStatus(string id, OrderStatusDTO orderStatus)
+        //{
+        //    AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
+        //    if (accountDTO == null)
+        //    {
+        //        return StatusCode(401, "Unauthorized");
+        //    }
 
-            var order = await _dataContext.Orders.FindAsync(id);
-            order.StaffId = accountDTO.Id;
-            order.Status = orderStatus.Status;
-            if (orderStatus.Status == "Completed")
-            {
-                var orderItems = _dataContext.OrderItems.Where(oi => oi.OrderId == id).ToList();
-                foreach (var item in orderItems)
-                {
-                    var product = await _dataContext.Products.FindAsync(item.ProductId);
-                    product.Quantity = product.Quantity - item.Quantity;
-                    await _dataContext.SaveChangesAsync();
-                }
-            }
-            await _dataContext.SaveChangesAsync();
+        //    var order = await _dataContext.Orders.FindAsync(id);
+        //    order.StaffId = accountDTO.Id;
+        //    order.Status = orderStatus.Status;
+        //    if (orderStatus.Status == "Completed")
+        //    {
+        //        var orderItems = _dataContext.OrderItems.Where(oi => oi.OrderId == id).ToList();
+        //        foreach (var item in orderItems)
+        //        {
+        //            var product = await _dataContext.Products.FindAsync(item.ProductId);
+        //            product.Quantity = product.Quantity - item.Quantity;
+        //            await _dataContext.SaveChangesAsync();
+        //        }
+        //    }
+        //    await _dataContext.SaveChangesAsync();
 
-            return StatusCode(200, "Update Order Status Successfully");
-        }
+        //    return StatusCode(200, "Update Order Status Successfully");
+        //}
 
-        //Get All Order Of The System
-        [HttpGet("all")]
-        public async Task<ActionResult> AllOrders()
-        {
-            AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
-            if (accountDTO == null)
-            {
-                return StatusCode(401, "Unauthorized");
-            }
-            var orders = await _dataContext.Orders.Include(o => o.ShippingAddress)
-                .Select(o => new
-                {
-                    id = o.Id,
-                    name = o.ShippingAddress.Name,
-                    phone = o.ShippingAddress.Phone,
-                    address = o.ShippingAddress.Address,
-                    city = o.ShippingAddress.City,
-                    country = o.ShippingAddress.Country,
-                    date = o.Date,
-                    shippingFee = o.ShippingFee,
-                    status = o.Status,
-                })
-                .ToListAsync();
-            return StatusCode(200, orders);
-        }
+        //[HttpGet("all")]
+        //public async Task<ActionResult> AllOrders()
+        //{
+        //    AccountDTO accountDTO = JwtHandler.ValiateToken(Request.HttpContext);
+        //    if (accountDTO == null)
+        //    {
+        //        return StatusCode(401, "Unauthorized");
+        //    }
+        //    var orders = await _dataContext.Orders.Include(o => o.ShippingAddress)
+        //        .Select(o => new
+        //        {
+        //            id = o.Id,
+        //            name = o.ShippingAddress.Name,
+        //            phone = o.ShippingAddress.Phone,
+        //            address = o.ShippingAddress.Address,
+        //            city = o.ShippingAddress.City,
+        //            country = o.ShippingAddress.Country,
+        //            date = o.Date,
+        //            shippingFee = o.ShippingFee,
+        //            status = o.Status,
+        //        })
+        //        .ToListAsync();
+        //    return StatusCode(200, orders);
+        //}
     }
 }
---
+++ Migrations/20230604095627_order.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _1670_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class order : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ShippingFee",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethod",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentMethod",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<double>(
+                name: "ShippingFee",
+                table: "Orders",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
